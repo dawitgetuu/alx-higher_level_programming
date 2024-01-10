d4d52d6516f5a8f556e725bec4a8b9585625cdd3
dawitgetuu <archfun3@gmail.com>
2024-01-10 09:37:34 -0800

    0x0B-python-input_output

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-Module to check if object is an instance of a
-class that inherited (directly or indirectly)
-from the specified class
-"""
+"""Defines an inherited class-checking function."""
 
 
 def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
     """
-    True if the object is an instance of a class that inherited
-    (directly or indirectly) from the specified class
-    """
-    if type(obj) == a_class:
-        return False
-    return isinstance(obj, a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
